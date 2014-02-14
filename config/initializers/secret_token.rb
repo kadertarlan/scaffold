7008d003fb33d9ab1151a7ffcd309ffb05136a14
kadertarlan <kadertarlan1@gmail.com>
2014-02-14 23:50:21 +0200

    duzenlendi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kader::Application.config.secret_key_base = '48d8904e59317b603aa1e397cb68e880a20634eed00ce05acfbb933f6b68058986923b3df016d067011bcc78eca1b094e13d15b4dbdbae885220cb66fd475bd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kader::Application.config.session_store :cookie_store, key: '_kader_session'
